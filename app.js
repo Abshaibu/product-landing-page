1ed8da3f40df3ff85c7c223d737a0c650834c377
abshaibu <shaibuabdulsalam56@gmail.com>
2022-05-10 18:01:44 +0100

    finished mobile view

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+// Global variables
 const menuBtn = document.querySelector('.menu');
+const cart = document.querySelector('.cart');
+const cartWrapper = document.querySelector('.cart-wrapper');
 const closeBtn = document.querySelector('.close');
 const navWrapper = document.querySelector('.nav-wrapper');
-menuBtn.addEventListener('click', () => {
+const quantityBtns = document.querySelector('.quantity-wrapper');
+
+// Menu Toggler
+menuBtn.addEventListener('click', (e) => {
+    navWrapper.classList.toggle('show-nav');
+})
+closeBtn.addEventListener('click', (e) => {
+    navWrapper.classList.toggle('show-nav');
+    e.stopPropagation();
+})
+navWrapper.addEventListener('click', (e) => {
     navWrapper.classList.toggle('show-nav');
 })
 
-closeBtn.addEventListener('click', () => {
-    navWrapper.classList.toggle('show-nav');
+// Cart Toggler
+cart.addEventListener('click', (e) => {
+    cartWrapper.classList.toggle('show-cart');
+    e.stopPropagation();
 })
 
-navWrapper.addEventListener('click', () => {
-    navWrapper.classList.toggle('show-nav');
+// Adding quantity to cart
+quantityBtns.addEventListener('click', (e) => {
+    if()
 })
