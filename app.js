8ef47bb395109ebae99a05eb6d159fa891067477
abshaibu <shaibuabdulsalam56@gmail.com>
2022-05-10 23:01:25 +0100

    added functionality to quantity buttons

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cart = document.querySelector('.cart');
 const cartWrapper = document.querySelector('.cart-wrapper');
 const closeBtn = document.querySelector('.close');
 const navWrapper = document.querySelector('.nav-wrapper');
-const quantityBtns = document.querySelector('.quantity-wrapper');
 
 // Menu Toggler
 menuBtn.addEventListener('click', (e) => {
@@ -24,7 +23,36 @@ cart.addEventListener('click', (e) => {
     e.stopPropagation();
 })
 
-// Adding quantity to cart
-quantityBtns.addEventListener('click', (e) => {
-
+// Choosing quantity to cart
+const cartBtns = document.querySelectorAll('.cart-btn');
+let count = 0;
+const qtyFigure = document.querySelector('.qty-figure');
+cartBtns.forEach( cartBtn => {
+    cartBtn.addEventListener('click', (e) => {
+        let clicked = e.target.classList;
+        if(clicked.contains('add')){
+            count++;
+        } else if(clicked.contains('subtract')){
+            if(qtyFigure.innerText <= 0){
+                count = 0;
+            } else {
+                count--;
+            }
+        }
+        qtyFigure.innerText = count;
+    })
 })
+
+// Adding quantity to cart counter
+// const cartCount = document.querySelector('.cart-count');
+// const addToCartBtn = document.querySelector('.add-to-cart-btn');
+// addToCartBtn.addEventListener('click', () => {
+//     if(qtyFigure <= 0){
+//
+//     } else {
+//         cartCount.style.display = 'block';
+//         cartCount.innerText = qtyFigure;
+//     }
+// })
+
+
